ac91e1d69958652d74ebba74bd8caf3f1f982c98
Ira Protasevich <iraprotasevich@yahoo.com>
2017-10-20 20:25:35 +0700

    первыйы коммит

---
+++ Epidem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epidem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Questionnaire()
+        {
+            return View();
+        }
+
+        public IActionResult QuestionnaireFinish()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
